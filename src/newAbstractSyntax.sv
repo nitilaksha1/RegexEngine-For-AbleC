2ee7cac14ee622adb4173d1a6d05185d1b55b2da
halak004 <halak004@umn.edu>
2017-04-06 15:40:37 -0500

    Update newAbstractSyntax.sv

--- src/newAbstractSyntax.sv
+++ src/newAbstractSyntax.sv
@@ -186,7 +186,7 @@ String ::= transitions::[Transition]
 		then ""
 		else
 			local attribute transition::Transition = head(transitions);
-			"(" ++ toString(fromState) ++ "," ++ toString(toState) ++ "," ++ transChar.lexeme ++ ")" ++ populatePP(tail(transitions));
+			"(" ++ toString(transition.fromState) ++ "," ++ toString(transition.toState) ++ "," ++ transChar.lexeme ++ ")" ++ populatePP(tail(transitions));
 
 }
 
